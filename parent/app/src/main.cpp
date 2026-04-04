3201d104a3f1063ca8e9081a33d39dbb2ba437ea
Svyatoslav Myshkovskiy <svatoslav190391@yandex.ru>
2026-04-04 16:13:26 +0300

    feat: добавил MainWindow для parent

--- parent/app/src/main.cpp
+++ parent/app/src/main.cpp
@@ -1,12 +1,11 @@
 #include <QApplication>
-#include <QMainWindow>
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QMainWindow w;
-    w.setWindowTitle("Parent");
+    MainWindow w;
     w.show();
 
     return a.exec();
